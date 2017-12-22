08ec7d4e8e406868ffd94adc7e8b3b184fbba408
NabeelDhukka <Nabeeldhukka@gmail.com>
2017-12-21 23:19:50 -0600

    test commit

--- app/src/main/java/com/example/nabee/instaclone/MainActivity.java
+++ app/src/main/java/com/example/nabee/instaclone/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.nabee.instaclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
     }
 }
